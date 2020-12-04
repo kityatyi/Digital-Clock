07cee91b9bd3534daf72b63ba6604ff57b8850b8
kityatyi <zsmonostori80@gmail.com>
2020-12-04 02:36:37 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Digital Clock
+
+Thank you for checking out my first ever JavaScript project that is worth mentioning, at least the first which actually does something useful. I have been learning JavaScript for about a month now using Mark Myers' book titled "A Smarter Way to Learn Javascript" as well as various other online resources such as video tutorials. This particular project was inspired by one of them.
+
+Initially, all I wanted was a working digital clock, without any graphic elements. Literally, all I wanted was the time in the desired format. Nothing more, nothing less. Then I started experimenting and added some CSS, which then resulted in a selectable dark and night theme. Later I also added the option to switch between 12 and 24-hour formats. Who knows what else will follow. I find these projects an excellent way to learn JavaScript. As an absolute beginner, I find them not only educational but also rewarding.
+
+Long story short, enjoy and feel free to further enhance it should you so wish.
+
+Zsolt
+
+zsmonostor80@gmail.com
+
---
+++ js/main.js
@@ -0,0 +1,100 @@
+var defaultTheme = true; // declares default color theme
+var timeFormat = true; // where true = 24h and false = 12h format
+showTime();
+
+// Switches between dark and light color theme
+function themeSwitcher() {
+  if (defaultTheme === true) {
+    document.getElementById("lnk").href = "css/style_light.css";
+    // changes button text to display "Dark theme"
+    defaultTheme = false;
+  } else {
+    document.getElementById("lnk").href = "css/style_dark.css";
+    defaultTheme = true;
+  }
+}
+
+// evaluates boolean and calls appropriate function
+function showTime() {
+  if (timeFormat === true) {
+    show24();
+  } else {
+    show12();
+  }
+}
+
+// switches between clock formats
+function formatSwitcher() {
+  //prettier-ignore
+  if (timeFormat === true) {
+    show12();
+    timeFormat = false;
+  }
+  // prettier-ignore
+  else {
+    show24();
+    timeFormat = true;
+  }
+}
+
+// 12-hour clock
+function show12() {
+  var date = new Date(); // Date object
+  var h = date.getHours(); // 0-23
+  var m = date.getMinutes(); // 0-59
+  var s = date.getSeconds(); // 0-59
+  var session = "AM";
+
+  if (h === 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12; // creates 12-hour system
+    session = "PM"; // appends PM if hour is greater than 12
+  }
+
+  if (h < 10) {
+    h = "0" + h; // appends "0" prefix if hour is less than 10
+  }
+
+  if (m < 10) {
+    m = "0" + m; // adds "0" prefix if minute is less than 10
+  }
+
+  if (s < 10) {
+    s = "0" + s; // adds "0" prefix if second is less than 10
+  }
+
+  var time = h + ":" + m + ":" + s + " " + session; // formats time into readable fashion
+
+  document.getElementById("myClockDisplay").innerText = time;
+  document.getElementById("myClockDisplay").textContent = time;
+  setTimeout(showTime, 1000);
+}
+
+// 24-hour clock
+function show24() {
+  var date = new Date(); // Date object
+  var h = date.getHours(); // 0-23
+  var m = date.getMinutes(); // 0-59
+  var s = date.getSeconds(); // 0-59
+
+  if (h < 10) {
+    h = "0" + h; // appends "0" prefix if hour is less than 10
+  }
+
+  if (m < 10) {
+    m = "0" + m; // adds "0" prefix if minute is less than 10
+  }
+
+  if (s < 10) {
+    s = "0" + s; // adds "0" prefix if second is less than 10
+  }
+
+  var time = h + ":" + m + ":" + s + " "; // formats time into readable fashion
+
+  document.getElementById("myClockDisplay").innerText = time;
+  document.getElementById("myClockDisplay").textContent = time;
+  setTimeout(showTime, 1000);
+}
